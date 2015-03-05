87f1d4d79f3a6c8cdfc3be38233276a88b254f90
Mariko Hisakawa <avenafatua@gmail.com>
2015-03-06 02:58:26 +0900

    Fix build shell script

--- chrome/build_makers_toy.sh
+++ chrome/build_makers_toy.sh
@@ -1,9 +1,18 @@
 rm -rf build_ninjaPCR;
+rm -f MakersToyPCR.zip;
+rm -rf build_MakersToyPCR;
 cp -r NinjaPCR  build_MakersToyPCR;
 
 # Replace "NinjaPCR" with "Makers Toy PCR"
-find ./build_MakersToyPCR/ -type f | xargs gsed -i "" 's/NinjaPCR/Makers Toy PCR/g'
+find ./build_MakersToyPCR -type f -print | xargs sed -i "" "s/NinjaPCR/Makers Toy PCR/g"
 
 zip -r MakersToyPCR.zip build_MakersToyPCR
 
-open https://chrome.google.com/webstore/developer/dashboard
+#open https://chrome.google.com/webstore/developer/dashboard
+
+echo "----------------------------------";
+
+grep "pcr_localize_appTitle" build_MakersToyPCR/PCR.html | gsed -e "s/.*>\(.*\)<.*/\1/g"
+grep "version" build_MakersToyPCR/manifest.json | gsed -e "s/.*\"\([0-9.]\+\)\".*/Version: \1/"
+
+echo "----------------------------------";
