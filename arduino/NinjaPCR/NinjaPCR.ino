827b66ab07136233980cec5b478019b1e086c4cc
Mariko Hisakawa <avenafatua@gmail.com>
2017-10-05 14:05:14 +0900

    Refactor / Implement IP address notification system

--- arduino/NinjaPCR/NinjaPCR.ino
+++ arduino/NinjaPCR/NinjaPCR.ino
@@ -22,7 +22,6 @@
 #include "pcr_includes.h"
 #include "thermocycler.h"
 
-
 #ifdef USE_WIFI
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
@@ -35,135 +34,124 @@
 Thermocycler* gpThermocycler = NULL;
 WifiCommunicator *wifi = NULL;
 
-boolean InitialStart() {
-  for (int i = 0; i < 50; i++) {
-    if (EEPROM.read(i) != 0xFF)
-      return false;
-  }
-  
-  return true;
+boolean isInitialStart() {
+    for (int i = 0; i < 50; i++) {
+        if (EEPROM.read(i) != 0xFF)
+            return false;
+    }
+    return true;
 }
 
-const SPIDTuning LID_PID_GAIN_SCHEDULE2[] = {
-  //maxTemp, kP, kI, kD
-  { 
-    70, 40, 0.15, 60   }
-  ,
-  { 
-    200, 80, 1.1, 10   }
-};
-void setup() {
+const SPIDTuning LID_PID_GAIN_SCHEDULE2[] =
+    {
+    //maxTemp, kP, kI, kD
+                { 70, 40, 0.15, 60 },
+                { 200, 80, 1.1, 10 } };
 
-  Serial.begin(BAUD_RATE);
+
 bool isApMode = false;
-#ifdef USE_WIFI
-  Serial.println("Starting NinjaPCR WiFi");
-  pinMode(PIN_WIFI_MODE, INPUT);
-  if (digitalRead(PIN_WIFI_MODE)==VALUE_WIFI_MODE_AP) {
-    Serial.println("Starting NinjaPCR WiFi with AP mode.");
-    isApMode = true;
-  } else {
-    Serial.println("Starting NinjaPCR WiFi with Normal mode.");
-  }
-#endif /* USE_WIFI */
+
+void setup() {
+    
+    Serial.begin(BAUD_RATE);
 
 #ifdef USE_WIFI
-  if (isApMode) {
-      setup_ap_mode();
-  } else {
-      setup_server_mode();
-      setup_normal();
-      wifi = new WifiCommunicator(wifi_receive, wifi_send);
-      gpThermocycler->SetCommunicator(wifi);
-      Serial.println("Starting WiFi...");
-      network_start();
-  }
+    Serial.println("Starting NinjaPCR WiFi");
+    pinMode(PIN_WIFI_MODE, INPUT);
+    isApMode = (digitalRead(PIN_WIFI_MODE)==VALUE_WIFI_MODE_AP);
+    if (isApMode) {
+        startWiFiAPMode();
+    }
+    else {
+        setup_normal();
+        wifi = new WifiCommunicator(wifi_receive, wifi_send);
+        gpThermocycler->SetCommunicator(wifi);
+        Serial.println("Starting WiFi...");
+        startWiFiHTTPServer();
+    }
 #else
-  setup_normal();
+    setup_normal();
 #endif
 }
 
-void setup_normal () {
+void setup_normal() {
 
 #ifdef USE_STATUS_PINS
-  pinMode(PIN_STATUS_A,OUTPUT);
-  pinMode(PIN_STATUS_B,OUTPUT);
-  digitalWrite(PIN_STATUS_A,LOW);
-  digitalWrite(PIN_STATUS_B,LOW);
+    pinMode(PIN_STATUS_A,OUTPUT);
+    pinMode(PIN_STATUS_B,OUTPUT);
+    digitalWrite(PIN_STATUS_A,LOW);
+    digitalWrite(PIN_STATUS_B,LOW);
 #endif /* USE_STATUS_PINS */
 
-  //init factory settings
-  if (InitialStart()) {
-    EEPROM.write(0, 100); // set contrast to 100
-  }
-  
-  //restart detection
-  
+    //init factory settings
+    if (isInitialStart()) {
+        EEPROM.write(0, 100); // set contrast to 100
+    }
+    //restart detection
+
 #ifdef USE_WIFI
-  //TODO EEPROM
-  boolean restarted = false;
+    //TODO EEPROM
+    boolean restarted = false;
 #else
-  boolean restarted = !(MCUSR & 1);
-  MCUSR &= 0xFE;
+    boolean restarted = !(MCUSR & 1);
+    MCUSR &= 0xFE;
 #endif /* USE_WIFI */
 
 #ifdef USE_STATUS_PINS
-  digitalWrite(PIN_STATUS_A, HIGH);
+    digitalWrite(PIN_STATUS_A, HIGH);
 #endif /* USE_STATUS_PINS */
-  
-  Serial.println("Init Thermocycler 0");
-  gpThermocycler = new Thermocycler(restarted);
-  Serial.println("Init Thermocycler 1");
-  
+
+    Serial.println("Init Thermocycler 0");
+    gpThermocycler = new Thermocycler(restarted);
+    Serial.println("Init Thermocycler 1");
+
 }
- 
-bool connected = false;
+
+bool isSerialConnected = false;
 bool initDone = false;
 short INTERVAL_MSEC = 500;
 
 void loop() {
 #ifdef USE_WIFI
     if (isApMode) {
-        loop_ap_mode();
+        loopWiFiAP();
         return;
     }
-#endif /* USE_WIFI */
-#ifdef USE_WIFI
-  if (network_is_connected()) {
-#else
-  if (connected) {
-#endif /* USE_WIFI */
     gpThermocycler->Loop();
     delay(1000);
-  } else {
-#ifndef USE_WIFI
-  checkPlugged();
-#endif /* USE_WIFI */
-  }
-#ifdef USE_WIFI
-  network_loop();
-#endif /* USE_WIFI */
+    if (isWiFiConnected()) {
+        loopWiFiHTTPServer();
+    } else {
+        Serial.println("WiFi Disconnected");
+    }
+#else
+    if (isSerialConnected) {
+        gpThermocycler->Loop();
+        delay(1000);
+    }
+    else {
+        checkSerialConnection();
+    }
 }
 
 bool startLamp = false;
-void checkPlugged () {
+void checkSerialConnection() {
     Serial.print("pcr1.0.5");
     Serial.print("\n");
- #ifdef USE_STATUS_PINS
+#ifdef USE_STATUS_PINS
     digitalWrite(PIN_STATUS_A, (startLamp)?HIGH:LOW);
 #endif /* USE_STATUS_PINS */
     startLamp = !startLamp;
     int timeStart = millis();
-    while (millis()<timeStart+INTERVAL_MSEC) {
-      while (Serial.available()){
-        char ch = Serial.read();
-        if (ch=='a'&&!connected) {
- #ifdef USE_STATUS_PINS
-          digitalWrite(PIN_STATUS_A, LOW);
+    while (millis() < timeStart + INTERVAL_MSEC) {
+        while (Serial.available()) {
+            char ch = Serial.read();
+            if (ch == 'a' && !isSerialConnected) {
+#ifdef USE_STATUS_PINS
+                digitalWrite(PIN_STATUS_A, LOW);
 #endif /* USE_STATUS_PINS */
-          
-          connected = true;
+                isSerialConnected = true;
+            }
         }
-      }
     }
 }
